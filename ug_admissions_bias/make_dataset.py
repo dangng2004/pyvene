39d26ff92f91f4f1b0b23667ee5d4a9e1a2b39f3
Dang Minh Nguyen <dangnguyen@indigo.cs.uchicago.edu>
2024-06-24 08:53:24 -0500

    adding scripts to create dataset and train an alignment for the college admissions bias task

---
+++ ug_admissions_bias/make_dataset.py
@@ -0,0 +1,282 @@
+import numpy as np
+import random
+import argparse
+import os
+from datasets import Dataset
+import pandas as pd
+from typing import Union
+
+BIOS_SETTINGS_SHORT = {
+    'race': ['White', 'Black', 'Latino', 'Asian'],
+    'gpa': np.arange(1.0, 4.01, step=0.01),
+    'num_ecs': np.arange(0, 9, step=1),
+    'num_letters': [0, 1, 2, 3],
+}
+
+BIOS_SETTINGS = {
+    'gender': ['male', 'female'],
+    'race': ['white', 'black', 'latino', 'asian'],
+    'income': [50, 100, 200, 400],
+    'geography': ['rural America', 'urban America', 'outside the U.S.'],
+    'school': ['private', 'public'],
+    'gpa': [2.0, 3.0, 3.5, 3.8, 4.0],
+    'sat': [1200, 1300, 1400, 1500, 1550, 1600],
+    'num_ecs': [1, 2, 4, 8],
+    'character_index': [1, 2, 3], # should there be a smart index?
+    'letters_quality': {
+        1: ['2 weak', '1 good, 1 weak'],
+        2: ['1 strong, 1 weak', '2 good'],
+        3: ['1 strong, 1 good', '2 strong'],
+    },
+    'topic': {
+        1: [
+            'learning to cook basic meals through following online recipes',
+            'learning to knit scarves through watching YouTube tutorials',
+            'learning to skateboard through practicing in the neighborhood park',
+            'learning to manage personal finances through budgeting my allowance',
+            'learning to take care of pets through owning a goldfish or hamster',
+        ],
+        2: [
+            'learning to launch a startup through entrepreneurial ventures',
+            'learning to navigate uncharted waters through solo sailing adventures',
+            'learning to innovate in sustainable design through eco-friendly projects',
+            'learning to excel in robotics through a state competition',
+            'learning to analyze complex data through scientific research projects',
+        ],
+        3: [
+            'learning to develop groundbreaking medical technology through doing summer field research',
+            'learning to compose award-winning symphonies through intensive music composition programs',
+            'learning to lead international humanitarian missions through nonprofit organizations',
+            'learning to represent my country in the Olympic Games through exceptional athletic prowess',
+            'learning to win international mathematics competitions through rigorous training and dedication',
+        ]
+    }
+}
+
+def is_diversity_admit(applicant):
+    gpa = applicant['gpa']
+    char_idx = applicant['character_index']
+    race = applicant['race']
+    income = applicant['income']
+    geo = applicant['geography']
+    
+    if gpa >= 3.8 and char_idx >= 2:
+        return True
+    elif gpa < 3.8 and gpa >= 3.5 and char_idx >= 2 and \
+    (race != 'white' or income <= 100 or geo != 'outside the U.S.'):
+        return True
+    elif gpa >= 3.8 and char_idx == 1 and \
+    (race != 'white' or income <= 100 or geo != 'outside the U.S.'):
+        return True
+    elif gpa < 3.5 and gpa >= 3.0 and char_idx == 1 and \
+    race == 'black':
+        return True
+    elif gpa < 3.0 and char_idx == 1 and \
+    race == 'black' and income <= 50:
+        return True
+    else:
+        return False
+
+def is_diversity_admit_short(candidate):
+    race = candidate['race']
+    gpa = candidate['gpa']
+    num_letters = candidate['num_letters']
+    num_ecs = candidate['num_ecs']
+    
+    if race != 'White' and num_ecs >= 1:
+        if num_letters >= 2 and gpa >= 3.0:
+            return True
+        elif num_letters == 1 and gpa >= 3.6:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+"""
+Sample a base and source input for training DAS.
+"""
+def sample_one_ctf(settings, ctf_behavior=None):
+    if ctf_behavior == None:
+        ctf_behavior = random.choice(['t->t', 't->f', 'f->t', 'f->f'])
+        
+    minorities = ['Black', 'Latino', 'Asian']
+    base_settings = {}
+    
+    # The left variable is just {race}
+    right_true = sample_bios_short_right(settings, True)
+    right_false = sample_bios_short_right(settings, False)
+    
+    # The idea is to sample base and source {race} first,
+    # then sample the same {num_letters}, {gpa}, and {num_ecs}
+    if ctf_behavior == 'f->f':
+        right_val = random.choice([True, False])
+        if right_val:
+            base_settings['num_letters'] = right_true[0]
+            base_settings['gpa'] = right_true[1]
+            base_settings['num_ecs'] = right_true[2]
+            
+            base_race = src_race = 'White'
+        else:
+            base_settings['num_letters'] = right_false[0]
+            base_settings['gpa'] = right_false[1]
+            base_settings['num_ecs'] = right_false[2]
+            
+            base_race = random.choice(settings['race'])
+            src_race = random.choice(settings['race'])
+            
+        base_label = src_label = 'No'
+    else:
+        base_settings['num_letters'] = right_true[0]
+        base_settings['gpa'] = right_true[1]
+        base_settings['num_ecs'] = right_true[2]
+        
+        if ctf_behavior == 't->t':
+            base_race = random.choice(minorities)
+            src_race = random.choice(minorities)
+            base_label = src_label = 'Yes'
+        elif ctf_behavior == 't->f':
+            base_race = random.choice(minorities)
+            src_race = 'White'
+            base_label = 'Yes'
+            src_label = 'No'
+        elif ctf_behavior == 'f->t':
+            base_race = 'White'
+            src_race = random.choice(minorities)
+            base_label = 'No'
+            src_label = 'Yes'
+            
+    base_settings['race'] = base_race
+    src_settings = base_settings.copy()
+    src_settings['race'] = src_race
+    
+    return base_settings, src_settings, base_label, src_label
+
+"""
+Sample an input that leads the right variable of the short
+admissions causal model to take on <value>.
+"""
+def sample_bios_short_right(settings, value: bool):
+    letters = settings['num_letters']
+    gpas = settings['gpa']
+    num_ecs = settings['num_ecs']
+    
+    letter = random.choice(letters)
+    gpa = random.choice(gpas)
+    
+    if value:
+        while not ((letter >= 2 and gpa >= 3.0) or \
+                    (letter == 1 and gpa >= 3.6)):
+            letter = random.choice(letters)
+            gpa = random.choice(gpas)
+        num_ec = random.choice(num_ecs[1:])
+    else:
+        while (letter >= 2 and gpa >= 3.0) or \
+                (letter == 1 and gpa >= 3.6):
+            letter = random.choice(letters)
+            gpa = random.choice(gpas)
+        num_ec = random.choice(num_ecs)
+    
+    return letter, gpa, num_ec
+        
+def sample_one(settings, custom_stats=None):
+    candidate = {}
+    for key in settings:
+        if custom_stats and key in custom_stats:
+            candidate[key] = custom_stats[key]
+        else:
+            if key == 'letters_quality' or key == 'topic':
+                char_index = candidate['character_index']
+                candidate[key] = random.choice(settings[key][char_index])
+            else:
+                candidate[key] = random.choice(settings[key])
+        
+    if 'gender' in candidate.keys():
+        if candidate['gender'] == 'male':
+            candidate['pronoun'] = 'he'
+            candidate['pronoun_pos'] = 'his'
+        else:
+            candidate['pronoun'] = 'she'
+            candidate['pronoun_pos'] = 'her'
+        
+    if 'num_pres' in candidate.keys():
+        candidate['num_pres'] = random.choice(np.arange(candidate['num_ecs']))
+        
+    return candidate
+
+def format_prompt(template, candidate, 
+                  dataset: Union['full', 'short'] = 'full'):
+    if dataset == 'full':
+        prompt = template.format(
+            pronoun_pos = candidate['pronoun_pos'],
+            pronoun = candidate['pronoun'],
+            gender = candidate['gender'],
+            race = candidate['race'],
+            income = candidate['income'],
+            geography = candidate['geography'],
+            school = candidate['school'],
+            gpa = candidate['gpa'],
+            sat = candidate['sat'],
+            num_ecs = candidate['num_ecs'],
+            num_pres = candidate['num_pres'],
+            letters_quality = candidate['letters_quality'],
+            topic = candidate['topic']
+        )
+    else:
+        prompt = template.format(
+            race = candidate['race'],
+            gpa = candidate['gpa'],
+            num_ecs = candidate['num_ecs'],
+            num_letters = candidate['num_letters']
+        )
+    return prompt
+
+"""
+Tokenize the label. The returned token index is somewhat
+arbitrary because it depends on the tokenizer. In this case
+it is the LLaMA tokenizer.
+"""
+def format_label(label_eng):
+    if label_eng == 'Yes':
+        return 8241
+    else:
+        return 3782
+
+"""SCRIPT STARTS HERE"""
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--dataset_size", help="Specify the size of the dataset.")
+parser.add_argument("--save_path", help="""Path to save the resulting dataset. 
+                                        Should end in a directory.""")
+
+args = parser.parse_args()
+ds_size = int(args.dataset_size)
+save_path = args.save_path
+
+template = open('./prompts/ug_admissions_short.txt').read()
+
+ctf_examples = [sample_one_ctf(BIOS_SETTINGS_SHORT) for _ in range(ds_size)]
+
+dataset_dict = {
+    'base': [format_prompt(template, ex[0], dataset='short') for ex in ctf_examples],
+    'source': [format_prompt(template, ex[1], dataset='short') for ex in ctf_examples],
+    'base_label': [format_label(ex[2]) for ex in ctf_examples],
+    'src_label': [format_label(ex[3]) for ex in ctf_examples]
+}
+
+dataset_all = Dataset.from_dict(dataset_dict)
+ds_train_test = dataset_all.train_test_split(test_size=0.2)
+ds_test = ds_train_test['test']
+ds_train_dev = ds_train_test['train'].train_test_split(test_size=0.2)
+ds_train = ds_train_dev['train']
+ds_dev = ds_train_dev['test']
+
+df_train = pd.DataFrame.from_dict(ds_train)
+df_dev = pd.DataFrame.from_dict(ds_dev)
+df_test = pd.DataFrame.from_dict(ds_test)
+
+os.makedirs(save_path, exist_ok=True)
+df_train.to_csv(os.path.join(save_path, "train.csv"), index=False)
+df_dev.to_csv(os.path.join(save_path, "dev.csv"), index=False)
+df_test.to_csv(os.path.join(save_path, "test.csv"), index=False)
---
+++ ug_admissions_bias/train_alignment.py
@@ -0,0 +1,211 @@
+import argparse
+import os
+from tqdm import tqdm
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torch.nn import CrossEntropyLoss
+from datasets import load_dataset
+from datasets import Dataset
+from transformers import get_linear_schedule_with_warmup, \
+LlamaForCausalLM, LlamaTokenizer, LlamaConfig
+
+import sys
+sys.path.append('..')
+import pyvene as pv
+from pyvene.models.intervenable_base import IntervenableModel
+from pyvene.models.interventions import BoundlessRotatedSpaceIntervention
+
+
+def create_llama(name="sharpbai/alpaca-7b-merged", 
+                cache_dir="../../.huggingface_cache"):
+    config = LlamaConfig.from_pretrained(name, cache_dir=cache_dir)
+    tokenizer = LlamaTokenizer.from_pretrained(name, 
+                                            cache_dir=cache_dir, 
+                                            padding_side='left')
+    llama = LlamaForCausalLM.from_pretrained(
+        name, config=config, cache_dir=cache_dir, 
+        torch_dtype=torch.bfloat16 # save memory
+    )
+    return config, tokenizer, llama
+
+"""
+Calculate cross entropy between logits and 
+a single target label (can be batched)
+"""
+def calculate_loss(logits, labels):
+    loss_fct = torch.nn.CrossEntropyLoss()
+    shift_labels = labels.to(logits.device)
+    loss = loss_fct(logits, shift_labels)
+    return loss
+
+def save_alignment(intervenable, args):
+    save_path = args.models_save_path
+    save_name = args.save_name
+
+    key = list(intervenable.interventions.keys())[0]
+    intervention_params = intervenable.interventions[key][0]
+
+    model_save_path = os.path.join(save_path, 
+                                    save_name + '/model.pt')
+    params_save_path = os.path.join(save_path, 
+                                    save_name + '/model_params.pt')
+
+    os.makedirs(os.path.dirname(model_save_path), exist_ok=True)
+    os.makedirs(os.path.dirname(params_save_path), exist_ok=True)
+
+    torch.save(intervenable.state_dict(), model_save_path)
+    torch.save(intervention_params.state_dict(), params_save_path)
+
+
+"""SCRIPT STARTS HERE"""
+
+device = 'cuda:0'
+num_epochs = 1
+batch_size = 32
+
+# layers = range(0, 30, 5)
+# positions = range(15, 50, 2) # race position is 16
+layers = range(0, 31, 5)
+positions = list(range(12, 25, 2)) + list(range(120, 125, 2))
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--dataset_path", help="""Path the the directory containing
+                    the dataset files""")
+parser.add_argument("--models_save_path", help="""Path to save the resulting models.
+                    Should end in a directory""")
+parser.add_argument("--results_save_path", help="""Path to the directory to save
+                    the dev accuracies""")
+# parser.add_argument("--save_name", help="""Name of the saved alignment""")
+
+args = parser.parse_args()
+
+_, tokenizer, llama = create_llama()
+_ = llama.to(device) # single gpu
+_ = llama.eval() # always no grad on the model
+
+ds_path = args.dataset_path
+ds = load_dataset('csv', data_files={
+    'train': os.path.join(ds_path, 'train.csv'),
+    'dev': os.path.join(ds_path, 'dev.csv'),
+    'test': os.path.join(ds_path, 'test.csv'),
+})
+
+train_loader = DataLoader(ds['train'], batch_size=batch_size)
+dev_loader = DataLoader(ds['dev'], batch_size=batch_size)
+test_loader = DataLoader(ds['test'], batch_size=batch_size)
+
+# we search over layers and token positions
+for layer in layers:
+    for position in positions:
+        args.save_name = f"layer_{layer}_pos_{position}"
+
+        config = pv.IntervenableConfig([
+            {
+                "layer": layer,
+                "component": 'block_output',
+                "intervention_type": pv.BoundlessRotatedSpaceIntervention,
+            }
+        ])
+        intervenable = pv.IntervenableModel(config, llama)
+        intervenable.set_device(device)
+        intervenable.disable_model_gradients()
+
+        # set up optimizer
+        total_steps = num_epochs * len(ds['train'])
+        optimizer_params = []
+        for k, v in intervenable.interventions.items():
+            try:
+                optimizer_params.append({
+                    "params": v[0].rotate_layer.parameters()
+                })
+                optimizer_params.append({
+                    'params': v[0].intervention_boundaries, 'lr': 1e-2
+                })
+            except:
+                pass
+        optimizer = torch.optim.Adam(optimizer_params, lr=1e-4)
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=int(0.1 * total_steps),
+            num_training_steps=total_steps,
+        ) 
+
+        # setting up tensorboard for loss visualization
+        tsboard_path = os.path.join('./tensorboard', args.save_name)
+        os.makedirs(tsboard_path, exist_ok=True)
+        writer = SummaryWriter(tsboard_path)
+
+        curr_step = 0
+        for epoch in range(num_epochs):
+            epoch_iterator = tqdm(
+                train_loader, desc=f"Epoch: {epoch}", position=0, leave=True
+            )
+            # training loop
+            for example in epoch_iterator:
+                base_tokens = tokenizer(example['base'], 
+                                        return_tensors='pt', 
+                                        padding=True).to(device)
+                source_tokens = tokenizer(example['source'], 
+                                        return_tensors='pt', 
+                                        padding=True).to(device)
+
+                _, counterfactual_outputs = intervenable(
+                    base_tokens,
+                    [source_tokens],
+                    {"sources->base": position},
+                )
+
+                logits = counterfactual_outputs.logits[:, -1]
+                loss = calculate_loss(logits, example['src_label'].to(device))
+                epoch_iterator.set_postfix({"loss": f"{loss.item():.3f}"})
+                
+                writer.add_scalar('training loss', loss, curr_step)
+
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+                
+                curr_step += 1
+
+            # eval
+            with torch.no_grad():
+                iterator = tqdm(dev_loader)
+                all_preds = []
+                all_labels = []
+                
+                for example in iterator:
+                    base_tokens = tokenizer(example['base'], 
+                                            return_tensors='pt', 
+                                            padding=True).to(device)
+                    source_tokens = tokenizer(example['source'], 
+                                            return_tensors='pt', 
+                                            padding=True).to(device)
+                
+                    _, counterfactual_outputs = intervenable(
+                        base_tokens,
+                        [source_tokens],
+                        {"sources->base": position},
+                    )
+
+                    logits = counterfactual_outputs.logits[:, -1]
+                    preds = logits.argmax(dim=-1).detach().cpu().numpy()
+                    all_preds.append(preds)
+                    all_labels.append(example['src_label'])
+                    
+                all_preds = np.concatenate(all_preds)
+                all_labels = np.concatenate(all_labels)
+                acc = accuracy_score(all_preds, all_labels)
+
+                writer.add_scalar('dev accuracy', acc, epoch)
+
+        # saving the model
+        save_alignment(intervenable, args)
+
+        os.makedirs(args.results_save_path, exist_ok=True)
+        with open(os.path.join(args.results_save_path, args.save_name + ".txt"), 'w') as fw:
+            fw.write(f"Final dev accuracy: {acc:.2f}")
